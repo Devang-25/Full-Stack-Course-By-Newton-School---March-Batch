287ca03ca05654d9499a2a6a184a0c4abe1ed338
Devang Sharma <devshu25@gmail.com>
2022-03-31 11:16:29 +0530

    Take Input Using Scanner Class

--- Main.java
+++ Main.java
@@ -207,7 +207,7 @@ public class Solution ---> Solution.java
 
 public class Devang ---> Devang.java
 
-(2)     public static void main(String[] args) {
+(2)  public static void main(String[] args) {
 
 main Function - DO NOT MODIFY this
 
@@ -227,7 +227,7 @@ System.out.println("Hello, Newton"); ---> Hello, Newton
 
 
 
-System: System Running youu Code -> IDE/Terminal/Online Editor (Environment)
+System: System Running your Code -> IDE/Terminal/Online Editor (Environment)
 out. : Output
 println: Print On Next Line
 
@@ -341,9 +341,12 @@ int a= "hello"; - INCORRECT
 Number(4), Decimal(2), Character(1), Boolean(1)
 
 
-(A) Character: 2 Bytes
+(A) Character: 2 Bytes 
 
-char: 'a'-'z', 'A'-'Z', 0-9, ',', ';' '!'  etc
+Other Languages: 1 Byte in Memory (C, C++, Py, Perl, Go)
+Java Language: 2 Byte in Memory
+
+char: 'a'-'z', 'A'-'Z', 0-9, ',', ';' '!', special character  etc
 
 Eg: 
 char a ='a'; - CORRECT
@@ -387,7 +390,7 @@ short a = 1000; - LESS Memory
 
 100.25
 
-Number = 25
+Number = 100
 Decimal= 0.25
 
 
@@ -411,6 +414,7 @@ In Memory, a = 223.25000000 (8 Digits Precision)
 
 
 ---> Formula to Calculate Range:
+
 Size in Bytes * 8 = Size in Bits = N
 
 Range = -2^(N-1) to 2(N-1)-1
@@ -456,8 +460,9 @@ Total Number of Ways =  2^32 Ways
 (2) Non-Primitive Data Types/User Defined
 
 
-
-
+- String
+- Array
+- Interfaces
 
 
 
@@ -469,8 +474,8 @@ public class Main {
     public static void main(String[] args) {
         char a = 'a';
         int i = 10;
-        float c = 12.5f;
-        double d = 32.5;
+        float c = 12.5f; // 12.5000
+        double d = 32.5; // 32.50000000
         long l = 1000L;
         Boolean b = true;
         short h = 5;
@@ -486,6 +491,367 @@ System.out.println(a + " " + i + " " + c + " "+ d + " " + l + " " + b + " " + h)
 OP:
 
 a 10 12.5 32.5 1000 true 5
+
+
+
+
+
+
+
+
+
+
+
+Date : 30th March 2022
+Mentor: DEVANG SHARMA
+Batch: March Batch - 2
+
+Agenda:
+- Revision: DONE
+- Doubts: Assignments: DONE
+- Decoding First Java Code: DONE
+- Variables and Constants: DONE
+- Data Types: DONE
+- How to Take Input from User in Java: DONE
+- Assignment: DONE
+
+- Operators
+- Loops
+
+
+"Please Type 'Hi' in the Chat Box if you have joined and Can See this Screen".
+
+
+
+
+
+
+
+
+
+
+----> How to Take Input from User in Java ?
+
+
+(1) Using Scanner Class
+(2) Using Buffered Reader
+
+
+
+---> Using Scanner Class:
+
+
+
+packages
+    - modules 
+        - classes/interfaces
+
+
+package = collections of modules
+module  = collections of classes
+
+
+java : package
+
+
+- io (input output) (Modules)
+    - bufferedreader
+    - scanner (FOCUS)
+    - customreader
+    - filereader
+
+- util (utility)
+    - apiexception
+    - collections
+    - collections framework
+    - comparator
+    - list
+    - map
+    - math
+
+- exceptions
+    - Arithemetic
+    - nullpointer
+
+
+
+How to Use a Class:
+
+import package.module.class
+
+
+import java.io.*;
+- Select All Classes inside io module of java package
+
+import java.util.*;
+- Select All Classes inside util module of java package
+
+
+
+
+Take Input from User:
+
+
+
+IP: 5
+OP: 10
+
+
+IP: n
+OP: 2*n
+
+
+
+Scanner Class
+
+
+Scanner sc = new Scanner(System.in);
+
+Scanner = Class
+sc = Object of Scanner Class
+
+
+System.in: Take Input from System
+
+System.out : Print Output to System
+
+
+Class obj;
+
+
+Class: Blueprint
+obj: Actual Instance
+
+
+Class: Cars
+object: BMW/Mercedes/Tesla/Audi...
+
+
+
+CODE:
+
+(1) How to take 1 Integer - DONE
+(2) How to take Multiple Integers - DONE
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); // 10
+        int b = sc.nextInt(); // 20
+        int c = sc.nextInt(); // 30
+
+        System.out.println(a + " " + b + " " + c);
+    }
+}
+
+
+IP:
+10 20 30 
+
+OP:
+10 20 30
+
+
+(3) How To Take Multiple Data Types
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); // 10
+        long l = sc.nextLong();
+        float f = sc.nextFloat();
+        double d = sc.nextDouble();
+        String s = sc.next();
+//        String s = sc.nextLine();
+
+
+        System.out.println(a + " " + l + " " + f + " "+ d + " " + s);
+    }
+}
+
+
+IP:
+10 10000 12.5 32.50 
+devang
+
+OP:
+10 10000 12.5 32.50 devang
+
+
+
+---> Take Character Inputs:
+
+        String s = sc.next(); // devang --> d:0,e:1,v:2,a:3,n:4,g:5
+        char first = s.charAt(0); // d
+        char second = s.charAt(1); // e
+        char third = s.charAt(2); // v
+
+        System.out.println(first + " " + second + " " + third);
+
+IP:
+devang
+
+OP:
+d e v
+
+
+
+
+
+
+
+
+
+Assignment - 2:
+
+Problem Statement
+
+Given two positive integers N and X, where N is the number of total patients and 
+X is the time duration (in minutes) after which a new patient arrives. 
+Also, doctor will give only 10 minutes to each patient. 
+The task is to calculate the time (in minutes) the last patient needs to wait.
+
+Input
+The first line of input contains the number of test cases T. The next subsequent lines denote the total number of patients N and time interval X (in minutes) in which the next patients are visiting.
+
+Constraints:
+
+1 <= T <= 100
+0 <= N <= 100
+0 <= X <= 30
+Output
+
+
+Output the waiting time of last patient.
+Example
+
+
+Input:
+5
+4 5
+5 3
+6 5
+7 6
+8 2
+
+Output:
+
+15
+28
+25
+24
+56
+
+
+Test Case-1:
+
+4 5
+
+
+Number of patients = N = 4
+
+
+A B C D
+
+
+Time for Each patient = X = 5
+
+
+A: 10-5 = 5
+B: 10-5 = 5
+C: 10-5 = 5
+
+D to wait = 15 Mins
+
+
+Test Case-2:
+
+5 3
+
+
+Number of patients = N = 5
+
+A B C D E
+
+Time for Each patient = X = 3
+
+A: 10-3 = 7
+B: 10-3 = 7
+C: 10-3 = 7
+D: 10-3 = 7
+
+E to wait = 28 Minutes
+
+
+Ans = (10-X)*(N-1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+------> Operators
+
+(1) Mathematical Operators:
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // *, +, -, / - Binary Operators: 2 Operands
+        // % 
+
+        int c = a + b;
+        int d = a * b;        
+        int e = a - b;        
+        int f = b/a;        
+
+        System.out.println(c + " " + d + " " + e + " " + f);
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
